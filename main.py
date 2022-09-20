914ad435c889393c264d743b0c9c63fcd45304e9
ezhou413 <evanzhou413@gmail.com>
2022-09-19 18:42:14 -0700

    completed data processing script

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import boto3
-import json
+import pandas as pd
 
-comprehend = boto3.client(service_name='comprehend', region_name='region')
+df = pd.read_csv('data/cao_data.csv')
+df['date'] = pd.to_datetime(df.get('date'))
 
-text = "It is raining today in Seattle"
+comprehend = boto3.client(service_name='comprehend', region_name='us-west-2')
 
-print('Calling DetectSentiment')
-print(json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
-print('End of DetectSentiment\n')
+def get_response(text):
+    #return json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4)['Sentiment']
+    response = comprehend.detect_sentiment(Text=text, LanguageCode='en')
+    return response
+
+df = df.assign(
+   response = df.get('description').apply(get_response)
+)
+
+def get_sentiment(text):
+    return text['Sentiment']
+    
+def get_sentiment_score(text):
+    return text['SentimentScore']
+
+df = df.assign(
+    sentiment = df.get('response').apply(get_sentiment),
+    sentiment_score = df.get('response').apply(get_sentiment_score)
+)
+
+df.to_csv('data/processed/cao.csv')
